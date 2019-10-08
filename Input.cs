2e1e5b01ccc1a5cf0aedf1c06fb0e71892edf28b
GhostYiL <51120128+GhostYiL@users.noreply.github.com>
2019-10-08 15:34:14 +0300

    Add files via upload

---
+++ Input.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRCTools;
+using VRC;
+using VRCModLoader;
+using VRCheat.Utils;
+
+namespace Mod.Teleport
+{
+    internal class InputB
+    {
+        public static void InputCancel()
+        {
+        }
+
+        public static void InputAccept(string str, List<KeyCode> keycodelist, Text text)
+        {
+            Player tptgt = PlayerUtils.Target(str);
+            inputcheck = str;
+            namecheck = PlayerUtils.GetAPIUser(tptgt).displayName;
+            VRCPlayer.Instance.transform.position = tptgt.transform.position;
+            MiscUtils.GetVRCUiManager().QueueHudMessage("Teleported to:" + PlayerUtils.GetAPIUser(tptgt).displayName + "!");
+            VRCUiManagerUtils.GetVRCUiManager().CloseUi(false);
+        }
+
+        public static void InputAttempt(string action)
+        {
+            InputB.InputCancelAction = new Action(InputB.InputCancel);
+            if (action == "Teleport")
+            {
+                InputB.InputAcceptAction = new Action<string, List<KeyCode>, Text>(InputB.InputAccept);
+            }
+            VRCUiPopupManagerUtils.GetVRCUiPopupManager().ShowUnityInputPopupWithCancel("Enter player name", "", InputField.InputType.Standard, false, "Teleport", InputB.InputAcceptAction, InputB.InputCancelAction, "Enter text....", true, null);
+        }
+
+        public static string namecheck;
+        public static string inputcheck;
+        public static Action InputCancelAction;
+        public static Action<string, List<KeyCode>, Text> InputAcceptAction;
+    }
+
+}
+
