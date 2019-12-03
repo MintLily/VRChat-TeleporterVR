51df90224a13cae82ab6ed2c38ec06690d88b71c
Janni9009 <33988244+Janni9009@users.noreply.github.com>
2019-12-03 14:00:36 +0100

    Opened Reloaded Branch

--- Input.cs
+++ Input.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 using VRCTools;
 using VRC;
 using VRCModLoader;
-using VRCheat.Utils;
 
 namespace Mod.Teleport
 {
@@ -15,26 +14,58 @@ namespace Mod.Teleport
         {
         }
 
-        public static void InputAccept(string str, List<KeyCode> keycodelist, Text text)
+        public static void InputAcceptTeleport(string str, List<KeyCode> keycodelist, Text text)
         {
             Player tptgt = PlayerUtils.Target(str);
             inputcheck = str;
             namecheck = PlayerUtils.GetAPIUser(tptgt).displayName;
             VRCPlayer.Instance.transform.position = tptgt.transform.position;
-            MiscUtils.GetVRCUiManager().QueueHudMessage("Teleported to:" + PlayerUtils.GetAPIUser(tptgt).displayName + "!");
+            VRCUiManagerUtils.GetVRCUiManager().CloseUi(false);
+        }
+
+        public static void InputAcceptCoords(string str, List<KeyCode> keycodelist, Text text)
+        {
+            string[] coords = str.Split(' ');
+            if (coords.Length == 3)
+            {
+                VRCPlayer.Instance.transform.position = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
+                PassedInputStageWithoutErrors = true;
+            }
+            if (coords.Length > 3)
+            {
+                VRCPlayer.Instance.transform.position = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
+                PassedInputStageWithoutErrors = true;
+                EasterEggTripped = true;
+            }
+            else
+            {
+                PassedInputStageWithoutErrors = false;
+            }
             VRCUiManagerUtils.GetVRCUiManager().CloseUi(false);
         }
 
         public static void InputAttempt(string action)
         {
-            InputB.InputCancelAction = new Action(InputB.InputCancel);
-            if (action == "Teleport")
+            InputCancelAction = new Action(InputCancel);
+            string raction = "";
+            if (action == "NTP")
             {
-                InputB.InputAcceptAction = new Action<string, List<KeyCode>, Text>(InputB.InputAccept);
+                InputAcceptAction = new Action<string, List<KeyCode>, Text>(InputAcceptTeleport);
+                TypeOfInput = "Enter (partial) playername:";
+                raction = "Teleport";
             }
-            VRCUiPopupManagerUtils.GetVRCUiPopupManager().ShowUnityInputPopupWithCancel("Enter player name", "", InputField.InputType.Standard, false, "Teleport", InputB.InputAcceptAction, InputB.InputCancelAction, "Enter text....", true, null);
+            if (action == "CoordTP")
+            {
+                InputAcceptAction = new Action<string, List<KeyCode>, Text>(InputAcceptCoords);
+                TypeOfInput = "Enter coords as X[Space]Y[Space]Z";
+                raction = "Teleport";
+            }
+            VRCUiPopupManagerUtils.GetVRCUiPopupManager().ShowUnityInputPopupWithCancel("TPVR-R Input:", "", InputField.InputType.Standard, false, raction, InputAcceptAction, InputCancelAction, TypeOfInput, true, null);
         }
 
+        public static bool EasterEggTripped = false; //Extra input? Hey, just like most of my code! :D
+        public static bool PassedInputStageWithoutErrors = false;
+        public static string TypeOfInput;
         public static string namecheck;
         public static string inputcheck;
         public static Action InputCancelAction;
