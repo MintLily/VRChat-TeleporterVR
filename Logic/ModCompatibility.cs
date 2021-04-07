dee14c37ed4b1eebb03338b774e7c7c64654d1c7
Korty (Lily) <admin@kortyboi.com>
2021-04-07 17:35:39 -0400

    Removed unneeded ModCompatibility

--- Logic/ModCompatibility.cs
+++ Logic/ModCompatibility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +9,11 @@ namespace TeleporterVR.Logic
 {
     class ModCompatibility
     {
-        public static bool DiscordMute = false;
+        //public static bool  = false;
 
         public static void Init()
         {
-            if (MelonHandler.Mods.FindIndex((MelonMod i) => i.Info.Name == "DiscordMute") != -1)
-                DiscordMute = true;
-            if (Main.isDebug && DiscordMute)
-                MelonLogger.Msg("Detected DiscordMute");
-
-            if (Main.isDebug)
-                MelonLoader.MelonLogger.Msg("Finished with Mod Compatibility");
         }
     }
 }
+*/
