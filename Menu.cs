dd920b354e019b9f228aa0152e6f659709bb93bc
Korty (Lily) <admin@kortyboi.com>
2021-05-10 20:54:09 -0400

    Changed and simplified things

--- Menu.cs
+++ Menu.cs
@@ -17,8 +17,7 @@ namespace TeleporterVR
     {
         public static QMNestedButton menu;
         public static QMSingleButton userSel_TPto;
-        private static QMSingleButton TPtoName;
-        private static QMSingleButton TPtoCoords;
+        private static QMSingleButton TPtoName, TPtoCoords;
         private static QMSingleButton SavePos1, SavePos2, SavePos3, SavePos4;
         private static QMSingleButton LoadPos1, LoadPos2, LoadPos3, LoadPos4;
         public static QMToggleButton VRTeleport;
@@ -44,96 +43,38 @@ namespace TeleporterVR
 
             VRTeleport = new QMToggleButton("ShortcutMenu", 3, -2, "VR", () =>
             {
-                VRUtils.active = true;
+                if (WorldActions.WorldAllowed)
+                    VRUtils.active = true;
             }, Language.theWord_Teleport, () =>
             {
-                VRUtils.active = false;
+                if (WorldActions.WorldAllowed)
+                    VRUtils.active = false;
             }, Language.perferedHand_Tooltip);
             VRTeleport.getGameObject().GetComponent<RectTransform>().anchoredPosition -= new Vector2(92f, 66.2f);
             VRTeleport.getGameObject().GetComponentInChildren<Transform>().localScale = new Vector3(0.6f, 0.75f, 1.0f);
             VRTeleport.getGameObject().name = BuildInfo.Name + "_VRTPToggleButton";
 
-            TPtoName = new QMSingleButton(menu, 1, 0, Logic.Language.TPtoName_Text, () =>
-            {
-                PopupManager.ShowInputPopup("Teleport to Player", "", InputField.InputType.Standard, false, "Teleport", 
-                    (s, __, ___) =>
-                    {
-                        Player tptgt = PlayerActions.Target(s);
-                        PopupManager.HideCurrentPopup(VRCUiPopupManager.prop_VRCUiPopupManager_0);
-                        if (tptgt != null)
-                            PlayerActions.GetLocalVRCPlayer().transform.position = tptgt.transform.position;
-                    }, null, "Enter (partial) Player Name");
-            }, Logic.Language.TPtoName_Tooltip);
+            TPtoName = new QMSingleButton(menu, 1, 0, Logic.Language.TPtoName_Text, () => OpenKeyboardForPlayerTP(), Logic.Language.TPtoName_Tooltip);
 
-            TPtoCoords = new QMSingleButton(menu, 2, 0, Logic.Language.TPtoCoord_Text, () =>
-            {
-                PopupManager.ShowInputPopup("Teleport to Postition", "", InputField.InputType.Standard, false, "Teleport",
-                    (s, __, ___) =>
-                    {
-                        string[] coords = s.Split(' ');
-                        if (coords.Length == 3)
-                        {
-                            PopupManager.HideCurrentPopup(VRCUiPopupManager.prop_VRCUiPopupManager_0);
-                            PlayerActions.GetLocalVRCPlayer().transform.position = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
-                        }
-                        else
-                            MelonLoader.MelonLogger.Error("Please input the correct coords as => X[space]Y[space]Z");
-                    }, null, "Enter coords as X[Space]Y[Space]Z");
-            }, Logic.Language.TPtoCoord_Tooltip);
+            TPtoCoords = new QMSingleButton(menu, 2, 0, Logic.Language.TPtoCoord_Text, () => OpenKeyboardForCoordTP(), Logic.Language.TPtoCoord_Tooltip);
 
-            perferdHand = new QMToggleButton(menu, 4, 0, "RightHanded", () => { VRUtils.preferRightHand = true; }, "LeftHanded", () => { VRUtils.preferRightHand = false; }, Logic.Language.perferedHand_Tooltip);
+            perferdHand = new QMToggleButton(menu, 4, 0, "Right Handed", () => { VRUtils.preferRightHand = true; }, "Left Handed", () => { VRUtils.preferRightHand = false; }, Logic.Language.perferedHand_Tooltip);
 
-            SavePos1 = new QMSingleButton(menu, 1, 1, Logic.Language.SavePos + "\n1", () =>
-            {
-                Pos1 = PlayerActions.GetLocalVRCPlayer().transform.position;
-                Rot1 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
-            }, Logic.Language.SavePos_ToolTip);
+            SavePos1 = new QMSingleButton(menu, 1, 1, Logic.Language.SavePos + "\n1", () => SaveAction(1), Logic.Language.SavePos_ToolTip);
 
-            SavePos2 = new QMSingleButton(menu, 2, 1, Logic.Language.SavePos + "\n2", () =>
-            {
-                Pos2 = PlayerActions.GetLocalVRCPlayer().transform.position;
-                Rot2 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
-            }, Logic.Language.SavePos_ToolTip);
+            SavePos2 = new QMSingleButton(menu, 2, 1, Logic.Language.SavePos + "\n2", () => SaveAction(2), Logic.Language.SavePos_ToolTip);
 
-            SavePos3 = new QMSingleButton(menu, 3, 1, Logic.Language.SavePos + "\n3", () =>
-            {
-                Pos3 = PlayerActions.GetLocalVRCPlayer().transform.position;
-                Rot3 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
-            }, Logic.Language.SavePos_ToolTip);
+            SavePos3 = new QMSingleButton(menu, 3, 1, Logic.Language.SavePos + "\n3", () => SaveAction(3), Logic.Language.SavePos_ToolTip);
 
-            SavePos4 = new QMSingleButton(menu, 4, 1, Logic.Language.SavePos + "\n4", () =>
-            {
-                Pos4 = PlayerActions.GetLocalVRCPlayer().transform.position;
-                Rot4 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
-            }, Logic.Language.SavePos_ToolTip);
+            SavePos4 = new QMSingleButton(menu, 4, 1, Logic.Language.SavePos + "\n4", () => SaveAction(4), Logic.Language.SavePos_ToolTip);
 
-            LoadPos1 = new QMSingleButton(menu, 1, 2, Logic.Language.LoadPos + "\n1", () =>
-            {
-                if (Pos1 == null || Rot1 == null) return;
-                PlayerActions.GetLocalVRCPlayer().transform.position = Pos1;
-                PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot1;
-            }, Logic.Language.LoadPos_Tooltip);
+            LoadPos1 = new QMSingleButton(menu, 1, 2, Logic.Language.LoadPos + "\n1", () => LoadAction(1), Logic.Language.LoadPos_Tooltip);
 
-            LoadPos2 = new QMSingleButton(menu, 2, 2, Logic.Language.LoadPos + "\n2", () =>
-            {
-                if (Pos2 == null || Rot2 == null) return;
-                PlayerActions.GetLocalVRCPlayer().transform.position = Pos2;
-                PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot2;
-            }, Logic.Language.LoadPos_Tooltip);
+            LoadPos2 = new QMSingleButton(menu, 2, 2, Logic.Language.LoadPos + "\n2", () => LoadAction(2), Logic.Language.LoadPos_Tooltip);
 
-            LoadPos3 = new QMSingleButton(menu, 3, 2, Logic.Language.LoadPos + "\n3", () =>
-            {
-                if (Pos3 == null || Rot3 == null) return;
-                PlayerActions.GetLocalVRCPlayer().transform.position = Pos3;
-                PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot3;
-            }, Logic.Language.LoadPos_Tooltip);
+            LoadPos3 = new QMSingleButton(menu, 3, 2, Logic.Language.LoadPos + "\n3", () => LoadAction(3), Logic.Language.LoadPos_Tooltip);
 
-            LoadPos4 = new QMSingleButton(menu, 4, 2, Logic.Language.LoadPos + "\n4", () =>
-            {
-                if (Pos4 == null || Rot4 == null) return;
-                PlayerActions.GetLocalVRCPlayer().transform.position = Pos4;
-                PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot4;
-            }, Logic.Language.LoadPos_Tooltip);
+            LoadPos4 = new QMSingleButton(menu, 4, 2, Logic.Language.LoadPos + "\n4", () => LoadAction(4), Logic.Language.LoadPos_Tooltip);
 
             menu.getMainButton().getGameObject().GetComponentInChildren<Text>().fontSize = 55;
             TPtoName.getGameObject().GetComponentInChildren<Text>().fontSize = 55;
@@ -146,10 +87,12 @@ namespace TeleporterVR
             LoadPos2.getGameObject().GetComponentInChildren<Text>().fontSize = 55;
             LoadPos3.getGameObject().GetComponentInChildren<Text>().fontSize = 55;
             LoadPos4.getGameObject().GetComponentInChildren<Text>().fontSize = 55;
+            perferdHand.getGameObject().GetComponentInChildren<Text>().fontSize = 55;
 
             MelonLoader.MelonCoroutines.Start(UpdateMenuIcon());
             UpdateUserSelectTeleportButton();
             UpdateVRTeleportButton();
+            UpdateLeftRightHandButton();
             perferdHand.setToggleState(Main.preferRightHand.Value);
 
             if (Main.isDebug)
@@ -159,6 +102,8 @@ namespace TeleporterVR
         public static void UpdateUserSelectTeleportButton() { userSel_TPto.setActive(Main.visible.Value); }
 
         public static void UpdateVRTeleportButton() { VRTeleport.setActive(Main.VRTeleportVisible.Value); }
+
+        public static void UpdateLeftRightHandButton() { perferdHand.setActive(Main.VRTeleportVisible.Value); }
 
         public static IEnumerator UpdateMenuIcon(bool ignoreWait = true)
         {
@@ -202,9 +147,96 @@ namespace TeleporterVR
 
             VRTeleport.setOffText(Language.theWord_Teleport);
             VRTeleport.setToolTip(Language.perferedHand_Tooltip);
+            perferdHand.setToolTip(Language.perferedHand_Tooltip);
+            perferdHand.setOnText(Language.preferedHanded_Text_ON);
+            perferdHand.setOffText(Language.preferedHanded_Text_OFF);
 
             if (Main.isDebug)
                 MelonLoader.MelonLogger.Msg("Updated button text and tooltip text");
+        }
+
+        public static void SaveAction(int slot)
+        {
+            switch (slot)
+            {
+                case 1:
+                    Pos1 = PlayerActions.GetLocalVRCPlayer().transform.position;
+                    Rot1 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
+                    break;
+                case 2:
+                    Pos2 = PlayerActions.GetLocalVRCPlayer().transform.position;
+                    Rot2 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
+                    break;
+                case 3:
+                    Pos3 = PlayerActions.GetLocalVRCPlayer().transform.position;
+                    Rot3 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
+                    break;
+                case 4:
+                    Pos4 = PlayerActions.GetLocalVRCPlayer().transform.position;
+                    Rot4 = PlayerActions.GetLocalVRCPlayer().transform.rotation;
+                    break;
+            }
+        }
+
+        public static void LoadAction(int slot)
+        {
+            switch (slot)
+            {
+                case 1:
+                    if (Pos1 == null || Rot1 == null) return;
+                    if (!WorldActions.WorldAllowed) return;
+                    PlayerActions.GetLocalVRCPlayer().transform.position = Pos1;
+                    PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot1;
+                    break;
+                case 2:
+                    if (Pos2 == null || Rot2 == null) return;
+                    if (!WorldActions.WorldAllowed) return;
+                    PlayerActions.GetLocalVRCPlayer().transform.position = Pos2;
+                    PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot2;
+                    break;
+                case 3:
+                    if (Pos3 == null || Rot3 == null) return;
+                    if (!WorldActions.WorldAllowed) return;
+                    PlayerActions.GetLocalVRCPlayer().transform.position = Pos3;
+                    PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot3;
+                    break;
+                case 4:
+                    if (Pos4 == null || Rot4 == null) return;
+                    if (!WorldActions.WorldAllowed) return;
+                    PlayerActions.GetLocalVRCPlayer().transform.position = Pos4;
+                    PlayerActions.GetLocalVRCPlayer().transform.rotation = Rot4;
+                    break;
+            }
+        }
+
+        public static void OpenKeyboardForPlayerTP()
+        {
+            PopupManager.ShowInputPopup("Teleport to Player", "", InputField.InputType.Standard, false, "Teleport",
+                    (s, __, ___) =>
+                    {
+                        Player tptgt = PlayerActions.Target(s);
+                        PopupManager.HideCurrentPopup(VRCUiPopupManager.prop_VRCUiPopupManager_0);
+                        if (tptgt != null)
+                            if (WorldActions.WorldAllowed)
+                                PlayerActions.GetLocalVRCPlayer().transform.position = tptgt.transform.position;
+                    }, null, "Enter (partial) Player Name");
+        }
+
+        public static void OpenKeyboardForCoordTP()
+        {
+            PopupManager.ShowInputPopup("Teleport to Postition", "", InputField.InputType.Standard, false, "Teleport",
+                    (s, __, ___) =>
+                    {
+                        string[] coords = s.Split(' ');
+                        if (coords.Length == 3)
+                        {
+                            PopupManager.HideCurrentPopup(VRCUiPopupManager.prop_VRCUiPopupManager_0);
+                            if (WorldActions.WorldAllowed)
+                                PlayerActions.GetLocalVRCPlayer().transform.position = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
+                        }
+                        else
+                            MelonLoader.MelonLogger.Error("Please input the correct coords as => X[space]Y[space]Z");
+                    }, null, "Enter coords as X[Space]Y[Space]Z");
         }
     }
 }
