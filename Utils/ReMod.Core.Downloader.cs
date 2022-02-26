38c5b99244297fff47e0615007892dab08051b02
Lily <admin@mintlily.lgbt>
2022-02-26 18:40:22 -0500

    Added ReMod.Core for ButtonApi

---
+++ Utils/ReMod.Core.Downloader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using MelonLoader;
+
+namespace TeleporterVR.Utils {
+    public class ReMod_Core_Downloader {
+        internal static bool failed;
+        public static void LoadReModCore(out Assembly loadedAssembly) {
+            byte[] bytes = null;
+            var wc = new WebClient();
+            try {
+                bytes = wc.DownloadData($"https://github.com/RequiDev/ReModCE/releases/latest/download/ReMod.Core.dll");
+                loadedAssembly = Assembly.Load(bytes);
+                Main.Log("Successfully Loaded ReMod.Core", Main.isDebug);
+            }
+            catch (WebException e) {
+                failed = true;
+                Main.Logger.Error($"Unable to Load Core Dep ReModCore: {e}");
+            }
+            catch (BadImageFormatException e) {
+                failed = true;
+                loadedAssembly = null;
+            }
+            loadedAssembly = null;
+        }
+    }
+}
