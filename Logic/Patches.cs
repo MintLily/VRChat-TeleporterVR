7caab05423e1f77b2fd61eb6a848d20ac213a628
Lily <admin@mintlily.lgbt>
2022-04-22 12:11:15 -0400

    Update patches

--- Logic/Patches.cs
+++ Logic/Patches.cs
@@ -1,82 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
 using HarmonyLib;
-using UnityEngine;
-using MelonLoader;
-using UnhollowerRuntimeLib.XrefScans;
-using TeleporterVR;
 using TeleporterVR.Utils;
-using UnhollowerBaseLib;
 using Log = MelonLoader.MelonLogger;
 
 // Came from https://github.com/Psychloor/PlayerRotater/blob/master/PlayerRotater/ModPatches.cs
 //     &     https://github.com/ddakebono/BTKSANameplateFix/blob/master/BTKSANameplateMod.cs
-namespace TeleporterVR.Patches
-{
-    internal static class NewPatches
-    {
-        public static bool IsQMOpen, IsAMOpen;
-        public static Action OnWorldJoin, OnWorldLeave;
+namespace TeleporterVR.Patches;
 
-        public static void SetupPatches()
-        {
-            bool d = Main.isDebug;
-            Main.Logger.Msg("Applying Patches . . .");
+internal static class NewPatches {
+    public static bool IsQmOpen, IsAmOpen;
 
-            applyPatches(typeof(LeftRoomPatches));
-            applyPatches(typeof(QuickMenuPatches));
+    public static void SetupPatches() {
+        Main.Log("Applying Patches . . .");
 
-            if (d) Main.Logger.Msg(ConsoleColor.Green, "Finished with Patches");
-        }
+        applyPatches(typeof(LeftRoomPatches));
+        applyPatches(typeof(QuickMenuPatches));
 
-        private static void applyPatches(Type type)
-        {
-            try {
-                if (Main.isDebug) Main.Logger.Msg($"Attempting {type.Name} Patches...");
-                HarmonyLib.Harmony.CreateAndPatchAll(type, "TeleporterVR");
-            } catch (Exception e) {
-                Main.Logger.Error($"Failed while patching {type.Name}!\n{e}");
-            }
-        }
+        Main.Log("Finished with Patches", Main.IsDebug);
     }
 
-
-    // Patch Methods
-
-    [HarmonyPatch(typeof(VRC.UI.Elements.QuickMenu))]
-    class QuickMenuPatches {
-        [HarmonyPostfix]
-        [HarmonyPatch("OnEnable")]
-        private static void OnQuickMenuEnable() {
-            NewPatches.IsQMOpen = true;
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch("OnDisable")]
-        private static void OnQuickMenuDisable() {
-            NewPatches.IsQMOpen = false;
+    private static void applyPatches(Type type) {
+        try {
+            Main.Log($"Attempting {type.Name} Patches...", Main.IsDebug);
+            HarmonyLib.Harmony.CreateAndPatchAll(type, "TeleporterVR");
+        } catch (Exception e) {
+            Main.Error($"Failed while patching {type.Name}!\n{e}");
         }
     }
+}
+
+
+// Patch Methods
+
+[HarmonyPatch(typeof(VRC.UI.Elements.QuickMenu))]
+internal class QuickMenuPatches {
+    [HarmonyPostfix]
+    [HarmonyPatch("OnEnable")]
+    private static void OnQuickMenuEnable() => NewPatches.IsQmOpen = true;
+
+    [HarmonyPostfix]
+    [HarmonyPatch("OnDisable")]
+    private static void OnQuickMenuDisable() => NewPatches.IsQmOpen = false;
+}
     
-    [HarmonyPatch(typeof(NetworkManager))]
-    class LeftRoomPatches {
-        [HarmonyPostfix]
-        [HarmonyPatch("OnLeftRoom")]
-        static void Yeet() {
-            WorldActions.OnLeftWorld();
-            NewPatches.OnWorldLeave?.Invoke();
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch("OnJoinedRoom")]
-        static void JoinedRoom() {
-            MelonCoroutines.Start(WorldActions.CheckWorld());
-            NewPatches.OnWorldJoin?.Invoke();
-        }
-    }
+[HarmonyPatch(typeof(NetworkManager))]
+internal class LeftRoomPatches {
+    [HarmonyPostfix]
+    [HarmonyPatch("OnLeftRoom")]
+    static void Yeet() => CheckWorldAllowed.OnWorldLeave();
 }
